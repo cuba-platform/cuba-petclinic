820b6040ae15eb5947c2999466828bc6f9eac397
Mario <mario@die-davids.de>
2018-10-23 23:29:11 +0200

    fixed pet CRUD screens with v7 APIs

--- modules/web/src/com/cubaplatform/petclinic/web/pet/pet/PetEdit.java
+++ modules/web/src/com/cubaplatform/petclinic/web/pet/pet/PetEdit.java
@@ -3,7 +3,7 @@ package com.cubaplatform.petclinic.web.pet.pet;
 import com.cubaplatform.petclinic.entity.pet.Pet;
 import com.haulmont.cuba.gui.screen.*;
 
-@UiController("sad_Pet.edit")
+@UiController("petclinic_Pet.edit")
 @UiDescriptor("pet-edit.xml")
 @EditedEntityContainer("petCt")
 public class PetEdit extends StandardEditor<Pet> {
