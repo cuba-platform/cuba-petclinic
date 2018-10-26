60ebc28ea764a96f668b4b3b1b18421c7033e149
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-10-26 18:23:28 +0400

    Open window and use injected components using v7 API

--- modules/web/src/com/cubaplatform/petclinic/web/pet/pet/PetBrowse.java
+++ modules/web/src/com/cubaplatform/petclinic/web/pet/pet/PetBrowse.java
@@ -1,14 +1,22 @@
 package com.cubaplatform.petclinic.web.pet.pet;
 
 import com.cubaplatform.petclinic.entity.pet.Pet;
-import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.Screens;
+import com.haulmont.cuba.gui.components.Action;
 import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
 
 @UiController("petclinic_Pet.browse")
 @UiDescriptor("pet-browse.xml")
 @LookupComponent("petsTable")
 public class PetBrowse extends StandardLookup<Pet> {
-    public void createDiseaseWarningMailing() {
-        openWindow("create-disease-warning-mailing", WindowManager.OpenType.DIALOG);
+
+    @Inject
+    private Screens screens;
+
+    @Subscribe("createDiseaseWarningMailing")
+    public void createDiseaseWarningMailing(Action.ActionPerformedEvent actionPerformedEvent) {
+        screens.create(CreateDiseaseWarningMailing.class, OpenMode.DIALOG).show();
     }
 }
