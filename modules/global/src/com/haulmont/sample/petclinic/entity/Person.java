82ef4228d4a1f2bd148c78cee072df4a3c157a2d
Mario <mario@die-davids.de>
2018-11-22 13:04:22 +0100

    added getName method for Person

--- modules/global/src/com/haulmont/sample/petclinic/entity/Person.java
+++ modules/global/src/com/haulmont/sample/petclinic/entity/Person.java
@@ -1,12 +1,14 @@
 package com.haulmont.sample.petclinic.entity;
 
+import com.haulmont.chile.core.annotations.MetaProperty;
 import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.StandardEntity;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
-@NamePattern("%s %s|firstName,lastName")
+@NamePattern("#getName|firstName,lastName")
 @MappedSuperclass
 public class Person extends StandardEntity {
     private static final long serialVersionUID = -2777766826323269523L;
@@ -35,4 +37,15 @@ public class Person extends StandardEntity {
     }
 
 
+    @Transient
+    @MetaProperty(related = "firstName,lastName")
+    public String getName() {
+        String name = firstName;
+
+        if (lastName != null) {
+            name += " " + lastName;
+        }
+
+        return name;
+    }
 }
