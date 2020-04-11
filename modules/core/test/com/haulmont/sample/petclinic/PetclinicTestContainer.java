f2fcc6112b97e0fe2ff9c91acefdfb4d6d8de38c
Mario David <mario@die-davids.de>
2020-04-11 15:26:21 +0200

    Run Integration Tests via Github Actions (#1)

--- modules/core/test/com/haulmont/sample/petclinic/PetclinicTestContainer.java
+++ modules/core/test/com/haulmont/sample/petclinic/PetclinicTestContainer.java
@@ -4,6 +4,7 @@ import com.haulmont.bali.util.Dom4j;
 import com.haulmont.cuba.testsupport.TestContainer;
 import org.dom4j.Document;
 import org.dom4j.Element;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -15,40 +16,14 @@ public class PetclinicTestContainer extends TestContainer {
         super();
         appComponents = new ArrayList<>(Arrays.asList(
                 "com.haulmont.cuba"
-                // add CUBA premium add-ons here
-                // "com.haulmont.bpm",
-                // "com.haulmont.charts",
-                // "com.haulmont.fts",
-                // "com.haulmont.reports",
-                // and custom app components if any
         ));
         appPropertiesFiles = Arrays.asList(
-                // List the files defined in your web.xml
-                // in appPropertiesConfig context parameter of the core module
-            "com/haulmont/sample/petclinic/app.properties",
-                // Add this file which is located in CUBA and defines some properties
-                // specifically for test environment. You can replace it with your own
-                // or add another one in the end.
-                "test-app.properties");
-        initDbProperties();
-    }
+                "com/haulmont/sample/petclinic/app.properties",
+                "com/haulmont/sample/petclinic/integrationtest-app.properties",
+                "com/haulmont/sample/petclinic/test-app.properties"
+        );
 
-    private void initDbProperties() {
-        File contextXmlFile = new File("modules/core/web/META-INF/context.xml");
-        if (!contextXmlFile.exists()) {
-            contextXmlFile = new File("web/META-INF/context.xml");
-        }
-        if (!contextXmlFile.exists()) {
-            throw new RuntimeException("Cannot find 'context.xml' file to read database connection properties. " +
-                    "You can set them explicitly in this method.");
-        }
-        Document contextXmlDoc = Dom4j.readDocument(contextXmlFile);
-        Element resourceElem = contextXmlDoc.getRootElement().element("Resource");
-
-        dbDriver = resourceElem.attributeValue("driverClassName");
-        dbUrl = resourceElem.attributeValue("url");
-        dbUser = resourceElem.attributeValue("username");
-        dbPassword = resourceElem.attributeValue("password");
+        autoConfigureDataSource();
     }
 
     public static class Common extends PetclinicTestContainer {
@@ -60,17 +35,19 @@ public class PetclinicTestContainer extends TestContainer {
         private Common() {
         }
 
+
         @Override
-        public void before() throws Throwable {
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
             if (!initialized) {
-                super.before();
+                super.beforeAll(extensionContext);
                 initialized = true;
             }
             setupContext();
         }
 
+        @SuppressWarnings("RedundantThrows")
         @Override
-        public void after() {
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
             cleanupContext();
             // never stops - do not call super
         }
