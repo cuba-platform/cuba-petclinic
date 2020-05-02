431789f0788d2b7f2d9b6a91c10be4d26106717c
Mario <mario@die-davids.de>
2020-05-02 20:30:11 +0200

    pet list in owner editor / browser (#21)

--- modules/global/src/com/haulmont/sample/petclinic/entity/owner/Owner.java
+++ modules/global/src/com/haulmont/sample/petclinic/entity/owner/Owner.java
@@ -1,14 +1,18 @@
 package com.haulmont.sample.petclinic.entity.owner;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Column;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import com.haulmont.chile.core.annotations.Composition;
+import com.haulmont.cuba.core.entity.annotation.OnDelete;
+import com.haulmont.cuba.core.global.DeletePolicy;
 import com.haulmont.sample.petclinic.entity.Person;
 import com.haulmont.sample.petclinic.entity.pet.Pet;
-import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Table(name = "PETCLINIC_OWNER")
 @Entity(name = "petclinic_Owner")
@@ -30,6 +34,8 @@ public class Owner extends Person {
     @Column(name = "TELEPHONE")
     protected String telephone;
 
+    @Composition
+    @OnDelete(DeletePolicy.CASCADE)
     @OneToMany(mappedBy = "owner")
     protected List<Pet> pets;
 
--- modules/web/src/com/haulmont/sample/petclinic/web/owner/owner/OwnerEdit.java
+++ modules/web/src/com/haulmont/sample/petclinic/web/owner/owner/OwnerEdit.java
@@ -1,8 +1,11 @@
 package com.haulmont.sample.petclinic.web.owner.owner;
 
 import com.haulmont.cuba.gui.Route;
+import com.haulmont.cuba.gui.components.Label;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.sample.petclinic.entity.owner.Owner;
+
+import javax.inject.Inject;
 
 @UiController("petclinic_Owner.edit")
 @UiDescriptor("owner-edit.xml")
@@ -10,4 +13,15 @@ import com.haulmont.sample.petclinic.entity.owner.Owner;
 @LoadDataBeforeShow
 @Route(value = "owners/edit", parentPrefix = "owners")
 public class OwnerEdit extends StandardEditor<Owner> {
+    @Inject
+    protected Label<String> titleLabel;
+    @Inject
+    protected MessageBundle messageBundle;
+
+    @Subscribe
+    protected void onAfterShow(AfterShowEvent event) {
+        titleLabel.setValue(messageBundle.formatMessage("ownerTitle", getEditedEntity().getName()));
+    }
+
+
 }
