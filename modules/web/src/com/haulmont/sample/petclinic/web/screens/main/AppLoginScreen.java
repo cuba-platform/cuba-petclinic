c405d1df6cb4c8b36759a842e5d96d30940ad183
Mario <mario@die-davids.de>
2020-05-05 07:22:18 +0200

    switch menu colors; secondary color: yellow

---
+++ modules/web/src/com/haulmont/sample/petclinic/web/screens/main/AppLoginScreen.java
@@ -0,0 +1,55 @@
+package com.haulmont.sample.petclinic.web.screens.main;
+
+import com.haulmont.cuba.gui.Route;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.web.app.login.LoginScreen;
+import com.haulmont.cuba.web.gui.screen.ScreenDependencyUtils;
+import com.vaadin.ui.Dependency;
+import javax.inject.Inject;
+
+
+@Route(path = "login", root = true)
+@UiController("login")
+@UiDescriptor("app-login-screen.xml")
+public class AppLoginScreen extends LoginScreen {
+
+    @Inject
+    protected HBoxLayout bottomPanel;
+
+    @Inject
+    protected Label<String> poweredByLink;
+
+    @Subscribe
+    public void onAppLoginScreenInit(InitEvent event) {
+        loadStyles();
+
+        initBottomPanel();
+    }
+
+    @Subscribe("submit")
+    public void onSubmit(Action.ActionPerformedEvent event) {
+        login();
+    }
+
+    protected void loadStyles() {
+        ScreenDependencyUtils.addScreenDependency(this,
+                "vaadin://brand-login-screen/login.css", Dependency.Type.STYLESHEET);
+    }
+
+    protected void initBottomPanel() {
+        if (!globalConfig.getLocaleSelectVisible()) {
+            poweredByLink.setAlignment(Component.Alignment.MIDDLE_CENTER);
+
+            if (!webConfig.getLoginDialogPoweredByLinkVisible()) {
+                bottomPanel.setVisible(false);
+            }
+        }
+    }
+
+    @Override
+    protected void initLogoImage() {
+        logoImage.setSource(RelativePathResource.class)
+                .setPath("VAADIN/brand-login-screen/cuba-icon-login.svg");
+    }
+}
