889acd48adc45f4efc7dd2530c40a83a21e94b60
Gleb Durygin <durygin@haulmont.com>
2020-09-16 14:57:57 +0400

    Wire timepartStart and timepartEnd components in VisitEditor

--- modules/web/src/test/groovy/com/haulmont/sample/petclinic/web/masquerade/ui/VisitEditor.groovy
+++ modules/web/src/test/groovy/com/haulmont/sample/petclinic/web/masquerade/ui/VisitEditor.groovy
@@ -2,27 +2,31 @@ package com.haulmont.sample.petclinic.web.masquerade.ui
 
 import com.haulmont.masquerade.Wire
 import com.haulmont.masquerade.base.Composite
-import com.haulmont.masquerade.components.Button
-import com.haulmont.masquerade.components.DateField
-import com.haulmont.masquerade.components.LookupField
-import com.haulmont.masquerade.components.LookupPickerField
-import com.haulmont.masquerade.components.TextArea
-import com.haulmont.masquerade.components.TimeField
+import com.haulmont.masquerade.components.*
+import com.haulmont.masquerade.sys.TagNames
+
+import static com.codeborne.selenide.Selenide.$
+import static com.haulmont.masquerade.Selectors.byChain
+import static com.haulmont.masquerade.Selectors.byCubaId
 
 @Wire(path = 'dialog_petclinic_Visit.edit')
-class VisitEditor extends Composite<VisitEditor>{
+class VisitEditor extends Composite<VisitEditor> {
 
     @Wire(path = ['visitStart', 'datepart'])
     public DateField datepartStart
 
-    @Wire(path = ['visitStart', 'timepart'])
-    public TimeField timepartStart
+    public TimeField timepartStart = $(byChain(byCubaId('visitStart'),
+            byCubaId('timepart'),
+            TagNames.DIV,
+            TagNames.INPUT)) as TimeField
 
-    @Wire(path =['visitEnd', 'datepart'])
-    public  DateField datepartEnd
+    @Wire(path = ['visitEnd', 'datepart'])
+    public DateField datepartEnd
 
-    @Wire(path = ["visitEnd", "timepart"])
-    public TimeField timepartEnd
+    public TimeField timepartEnd = $(byChain(byCubaId('visitEnd'),
+            byCubaId('timepart'),
+            TagNames.DIV,
+            TagNames.INPUT)) as TimeField
 
     @Wire
     public LookupPickerField pet
